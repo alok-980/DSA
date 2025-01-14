dc9b7bf12186641c2c0040fa3404360b49316746
Alok Chauhan <alokchuhan7896@gmail.com>
2025-01-14 13:04:58 +0530

    Add new quest trapping rainwater

--- Array_I/6_SubArray/Main.java
+++ Array_I/6_SubArray/Main.java
@@ -1,22 +1,43 @@
+// print SubArray, count the SubArray sum and find the MIN & MAX
+
 import java.util.*;
 
 public class Main {
 
-    public static void printSubArray(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            for(int j=i; j<arr.length; j++) {
-                for(int k=i; k<=j; k++) {
+    public static void findSubArray(int arr[]) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        //printing subarray
+        for(int i = 0; i<arr.length; i++) {
+            for(int j = i; j<arr.length; j++) {
+                System.out.print("(");
+                for(int k = i; k<=j; k++) {
                     System.out.print(arr[k] + " ");
+
+                    //counting the subarray sum
+                    sum = sum + arr[k];
                 }
-                System.out.println();
+                System.out.print(")");
+                System.out.print(" = " + sum + ",  ");
+
+                //finding MIN & MAX
+                if(min > sum) {
+                    min = sum;
+                } else if(max < sum) {
+                    max = sum;
+                }
+                sum = 0;
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println("min = " + min + " & " + "max = " + max);
     }
 
     public static void main(String args[]) {
-        int arr[] = {2, 4, 6, 8, 10};
-
-        printSubArray(arr);
+        int arr[] = {12, 4, 6, 8, 10, 40};
+        findSubArray(arr);
     }
 }
